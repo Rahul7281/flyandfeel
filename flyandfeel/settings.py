9c49763228a6eff0b7316cc71197a0600636d799
Rahul7281 <rahulsuthar7281@gmail.com>
2025-01-21 21:30:36 +0530

    updated code

--- flyandfeel/settings.py
+++ flyandfeel/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_DIRS = [os.path.join(BASE_DIR, 'assets')]
+# STATIC_DIRS = [os.path.join(BASE_DIR, 'assets')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
