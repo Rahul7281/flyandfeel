bde6f18ec897eb6ca7f61703e83e57a345e764b5
Rahul7281 <rahulsuthar7281@gmail.com>
2025-01-21 21:28:17 +0530

    updated code

--- flyandfeel/settings.py
+++ flyandfeel/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['flea-evolving-endlessly.ngrok-free.app','*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
+    # 'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    # 'whitenoise.middleware.WhitenoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
